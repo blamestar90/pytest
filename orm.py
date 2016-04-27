2c29795bb605aa7c0b8187ad4944e6564e352829
zxc <zxc199043@outlook.com>
2016-04-27 14:50:03 +0800

    day03-2

--- orm.py
+++ orm.py
@@ -8,7 +8,7 @@ def log(sql, args=()):
 
 @asyncio.coroutine
 def create_pool(loop, **kw):
-	# print(kw)
+    # print(kw)
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
@@ -62,70 +62,7 @@ def create_args_string(num):
     return ', '.join(L)
 
 
-class Model(dict, metaclass=ModelMetaclass):
-	
-
-	def __init__(self, **kw):
-	    super(Model, self).__init__(**kw)
-
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
-
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-
-    def getValue(self, key):
-        return getattr(self, key, None)
-
-
-    def getValueOrDefault(self, key):
-        value = getValue(self, key, None)
-        if value is None:
-            field = self.__mappings__[key]
-            if field.default is not None:
-                value = field.default() if callable(field.default) else field.default
-                logging.info('using default value for %s: %s' % (key, str(value)))
-                setattr(self, key, value)
-        return value
-
-
-    @classmethod
-    @asyncio.coroutine
-    def find(cls, pk):
-        ' find object by primary key. '
-        rs = yield from select('%s where `%s`=?' % (cls.__select__, 
-        	cls.__primary_key__), [pk], 1)
-        if len(rs) == 0:
-            return None
-        return cls(**rs[0])
-
-class Field(object):
-
-
-    def __init__(self, name, column_type, primary_key, default):
-        self.name = name
-        self.column_type = column_type
-        self.primary_key = primary_key
-        self.default = default
-
-
-    def __str__(self):
-        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-class StringField(Field):
-    
-
-    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-        super().__init__(name, ddl, primary_key, default)
-        
 class ModelMetaclass(type):
-    
 
     def __new__(cls, name, bases, attrs):
         # 排除Model类本身
@@ -143,10 +80,10 @@ class ModelMetaclass(type):
                 logging.info('    found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
-                	# 找到主键
-                	if primaryKey:
-                	    raise RuntimeError('Duplicate primary key for field: %s' % k)
-                	primaryKey = k
+                    # 找到主键
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
                 else:
                     fields.append(k)
         if not primaryKey:
@@ -168,3 +105,84 @@ class ModelMetaclass(type):
         	primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
+
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getValue(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.info('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        ' find object by primary key. '
+        rs = yield from select('%s where `%s`=?' % (cls.__select__,
+            cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
