bc28a8503a02bccf415dab6293e7a9c247667cae
zxc <zxc199043@outlook.com>
2016-05-06 17:24:40 +0800

    day08-2

---
+++ www/handlers.py
@@ -0,0 +1,13 @@
+from coreweb import get
+from models import User
+import asyncio
+
+
+@get('/')
+@asyncio.coroutine
+def index(request):
+    users = yield from User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
