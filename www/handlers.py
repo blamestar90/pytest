dd4cf0979fb1f717cb05a6dbb5e65f515ea4a23a
zxc <zxc199043@outlook.com>
2016-05-11 10:22:52 +0800

    day08-2

--- www/handlers.py
+++ www/handlers.py
@@ -1,13 +1,19 @@
 from coreweb import get
-from models import User
+from models import User, Blog
 import asyncio
+import time
 
 
 @get('/')
 @asyncio.coroutine
 def index(request):
-    users = yield from User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ipsam vero tempore magni nulla consequuntur, iusto fugit optio voluptates error eaque doloremque molestias exercitationem saepe repellendus maiores id. Natus, nostrum.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'users': blogs
     }
